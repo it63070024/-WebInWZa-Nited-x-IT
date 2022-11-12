9bb41e42d10640dfda5ac94b15781a90995987d7
it63070093 <63070093@it.kmitl.ac.th>
2022-11-12 21:48:54 +0700

    update welcome

--- welcome.js
+++ welcome.js
@@ -1,5 +1,13 @@
 
-    function newmap() {
-        location.replace("tutorial.html")
-      }
+
+function Open() {
+  document.querySelector(".OpenL").style.animation = "OpenL 3s forwards";
+  document.querySelector(".OpenR").style.animation = "OpenR 3s forwards";
+  setTimeout(newmap,5000);
+}
+
+
+function newmap() {
+  location.replace("tutorial.html");
+}
 
