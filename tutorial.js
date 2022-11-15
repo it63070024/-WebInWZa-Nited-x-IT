b677c49730892480b08fd877bd3afd917dba6050
it63070093 <63070093@it.kmitl.ac.th>
2022-11-15 19:40:40 +0700

    add tutorial walk

--- tutorial.js
+++ tutorial.js
@@ -1,6 +1,7 @@
-var y = 6;
-py = y+'%';
+var y = 18;
+py = y + 'vw';
 var m = 0;
+
 const bun = document.querySelector('.clickR') //ปุ่มที่จะใช้กด
 const bun1 = document.querySelector('.clickL')
 var walkRight = false; //เช็คเดินขวา
@@ -8,20 +9,40 @@ var walkLeft = false; //เช็คเดินซ้าย
 let timer
 sessionStorage.isMuted = false
 
+var front_back = 1;
+var turn = 0;
+var walk = 0;
+
+
+var check = 0;
+var collision = 0;
+var check_text = 0;
+
+
+
 const character = document.querySelector(".c1")
 const character_img = document.querySelector(".cha")
 const left = document.querySelector(".left")
 const right = document.querySelector(".right")
-const map2m = document.getElementById("map1")
+const map1m = document.getElementById("map1")
 character.style.setProperty('--moveset',py);
+
 const cha_offset = document.getElementById("c1");
 const check_offset = document.getElementById("check");
+const check_end = document.querySelector(".door");
+
+
+
+var mid = check_offset.offsetLeft - cha_offset.offsetLeft;
 
 document.addEventListener('keydown', function(event) {
     
-    if((y>=88)){
-      setTimeout(newmap, 1000);
-      
+    
+    mid = check_offset.offsetLeft - cha_offset.offsetLeft;
+
+    if ((cha_offset.offsetLeft >= check_end.offsetLeft)) {
+        check=1;
+        setTimeout(newmap, 500);
     }
 
     if((event.keyCode == 68 || event.keyCode == 39)){
@@ -33,47 +54,141 @@ document.addEventListener('keydown', function(event) {
         left.style.width = "2.7vw"
     }
     /*D*/
-    if((event.keyCode == 68 || event.keyCode == 39) && y<88){
-        y += 0.7;
-        py = y+'vw';
-        character.style.setProperty('--moveset',py);
-        character_img.setAttribute("src", "./PNG update/Map1/character/main_cha.gif")
-    }
-     
+    if ((event.keyCode == 68 || event.keyCode == 39) && (window.innerWidth * 0.50) + (cha_offset.offsetLeft) >= check_end.offsetLeft && cha_offset.offsetLeft <= check_end.offsetLeft && check == 0) {
+        y += 0.8;
+        py = y + 'vw';
+        
+        character.style.setProperty('--moveset', py);
 
-    else if((event.keyCode == 68 || event.keyCode == 39) && y < 88) { 
-        y += 0.7;
-        py = y+'vw';
-        character.style.setProperty('--moveset',py);
-        character_img.setAttribute("src", "./PNG update/Map1/character/main_cha.gif")
-    
-}       
-
-
-    
-    /*A*/
-    if((event.keyCode == 65 || event.keyCode == 37) && y >= 930 && m<-55){
-        y -= 0.7;
-        py = y+'vw';
-        character.style.setProperty('--moveset',py)
-        character_img.setAttribute("src", "./PNG update/Map1/character/main_cha_flip.gif")
-        console.log("1")
-    }
- 
-    else if((event.keyCode == 65 || event.keyCode == 37)) {
-        if (y > 1) {
-            y -= 0.7;
-                py = y+'vw';
-                character.style.setProperty('--moveset',py);
-                character_img.setAttribute("src", "./PNG update/Map1/character/main_cha_flip.gif")    
-            
+        walk += 1
+        front_back = Math.abs(front_back)
+        if (turn == 0) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1.PNG")
+            turn = 1
         }
-            
+        if (walk % 10 == 0) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1.PNG")
+        }
+        else if (walk % 10 == 5){
+            character_img.setAttribute("src", "./PNG update/character/Mc2.PNG")
+        }
+
+        console.log("1");
+
     }
 
-    if (m==0 && y==620) {
-        y+=1; /* เพิ่ม y 1 เพื่อออก if นี้ */
+    else if ((event.keyCode == 68 || event.keyCode == 39) && mid > check_offset.offsetLeft / 2 && check == 0) {
+        y += 0.8;
+        py = y + 'vw';
+        character.style.setProperty('--moveset', py);
+
+
+        front_back = Math.abs(front_back)
+        walk += 1
+        if (turn == 0) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1.PNG")
+            turn = 1
+        }
+        if (walk % 10 == 0) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1.PNG")
+        }
+        else if (walk % 10 == 5){
+            character_img.setAttribute("src", "./PNG update/character/Mc2.PNG")
+        }
+        
+        console.log("2")
+
     }
+    else if ((event.keyCode == 68 || event.keyCode == 39) && mid <= (check_offset.offsetLeft / 2) && check == 0) {
+        y += 0.8;
+        py = y + 'vw';
+        m -= 0.8;
+        my = m + 'vw';
+        map1m.style.setProperty('--map1move', my)
+        character.style.setProperty('--moveset', py);
+
+        walk += 1
+        front_back = Math.abs(front_back)
+        if (turn == 0) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1.PNG")
+            turn = 1
+        }
+        if (walk % 10 == 0) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1.PNG")
+        }
+        else if (walk % 10 == 5){
+            character_img.setAttribute("src", "./PNG update/character/Mc2.PNG")
+        }
+        console.log("3")
+    }
+
+
+    /*A*/
+    if ((event.keyCode == 65 || event.keyCode == 37) && cha_offset.offsetLeft < check_end.offsetLeft && cha_offset.offsetLeft+(window.innerWidth *0.50) >= check_end.offsetLeft && check == 0) {
+        y -= 0.8;
+        py = y + 'vw';
+        character.style.setProperty('--moveset', py)
+
+
+        front_back = -Math.abs(front_back)
+        walk += 1
+        if (turn == 1) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1_flip.PNG")
+            turn = 0
+        }
+        if (walk % 10 == 0) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1_flip.PNG")
+        }
+        else if (walk % 10 == 5){
+            character_img.setAttribute("src", "./PNG update/character/Mc2_flip.PNG")
+        }
+        
+        console.log("4")
+    }
+
+    else if ((event.keyCode == 65 || event.keyCode == 37) && y >= 0 && m < -1 && check == 0) {
+        y -= 0.8;
+        py = y + 'vw';
+        m += 0.8;
+        my = m + 'vw';
+        map1m.style.setProperty('--map1move', my)
+        character.style.setProperty('--moveset', py);
+        front_back = -Math.abs(front_back)
+        walk += 1
+        if (turn == 1) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1_flip.PNG")
+            turn = 0
+        }
+        if (walk % 10 == 0) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1_flip.PNG")
+        }
+        else if (walk % 10 == 5){
+            character_img.setAttribute("src", "./PNG update/character/Mc2_flip.PNG")
+        }
+        console.log("5")
+    }
+    else if ((event.keyCode == 65 || event.keyCode == 37) && y > 1 && check == 0) {
+
+        y -= 0.8;
+        py = y + 'vw';
+        character.style.setProperty('--moveset', py);
+
+        front_back = -Math.abs(front_back)
+        walk += 1
+        if (turn == 1) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1_flip.PNG")
+            turn = 0
+        }
+        if (walk % 10 == 0) {
+            character_img.setAttribute("src", "./PNG update/character/Mc1_flip.PNG")
+        }
+        else if (walk % 10 == 5){
+            character_img.setAttribute("src", "./PNG update/character/Mc2_flip.PNG")
+        }
+        console.log("6")
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
 
     
 })
@@ -82,11 +197,14 @@ document.addEventListener('keyup', function(event){
     if((event.keyCode == 68 || event.keyCode == 39)){
         right.setAttribute("src", "./PNG update/tutorial/right.png")
         right.style.width = "3vw"
+        character_img.setAttribute("src", "./PNG update/Map1/character/main_cha.gif")
     } 
     else if((event.keyCode == 65 || event.keyCode == 37)){
         left.setAttribute("src", "./PNG update/tutorial/left.png")
         left.style.width = "3vw"
+        character_img.setAttribute("src", "./PNG update/Map1/character/main_cha_flip.gif")
     }  
+
 })
     function newmap() {
         location.replace("map1.html")
