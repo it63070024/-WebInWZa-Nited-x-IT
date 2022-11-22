f036254ee5f764447ed74efaa9fecadee6a6c569
it63070024 <63070024@it.kmitl.ac.th>
2022-11-22 14:37:15 +0700

    welcome edit

--- welcome.js
+++ welcome.js
@@ -3,6 +3,8 @@ document.addEventListener('contextmenu', event => event.preventDefault()); //Rig
 function Open() {
   document.querySelectorAll(".Open")[0].style.animation = "OpenL 3s forwards";
   document.querySelectorAll(".Open")[1].style.animation = "OpenR 3s forwards";
+  document.querySelectorAll(".Open")[2].style.animation = "OpenL 3s forwards";
+  document.querySelectorAll(".Open")[3].style.animation = "OpenR 3s forwards";
   setTimeout(newmap,4500);
 }
 
